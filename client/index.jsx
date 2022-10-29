aaab3e0e0b8e9322fe009c5b7eac8b6849179e65
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-29 08:50:49 +0200

    React app for Azure

---
+++ client/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
+
+
+const root = createRoot(document.getElementById("app"));
+
+root.render(
+    <h1>Hello Class</h1>
+);
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ console.log('Hello World');
 
 const app = express();
 
+app.use(express.static("../client/dist"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
    console.log(`Started on http://localhost:${server.address().port}`);
 });
